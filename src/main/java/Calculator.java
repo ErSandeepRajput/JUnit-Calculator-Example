62fe4f11b5c5b8238590af4bc9b482a28cf98f72
Sandeep Rajput <github@rajputsandeep.com>
2018-06-11 17:51:03 +0530

    Edited comments of method of Calculator.java class at src/main/java directory

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,28 +1,40 @@
-/** Java class for Calculation of Two integers*/
+/**
+ * Java class for Calculation of Two integers
+ */
 class Calculator {
 
-    /** Calculator class constructor*/
+    /**
+     * Calculator class constructor
+     */
     protected Calculator(){
 
     }
 
-    /** Method for addition of Two Integers*/
-    static int addition(int a, int b) {
+    /**
+     * Method for addition of Two Integers
+     */
+    static int addition(final int a, final int b) {
         return a + b;
     }
 
-    /** Method for subtraction of Two Integers*/
-    static int subtraction(int a, int b) {
+    /**
+     * Method for subtraction of Two Integers
+     */
+    static int subtraction(final int a, final int b) {
         return a - b;
     }
 
-    /** Method for Multiplication of Two Integers*/
-    static int multiplication(int a, int b) {
+    /**
+     * Method for Multiplication of Two Integers
+     */
+    static int multiplication(final int a, final int b) {
         return a * b;
     }
 
-    /** Method for Division of Two Integers*/
-    static int division(int a, int b) {
+    /**
+     * Method for Division of Two Integers
+     */
+    static int division(final int a, final int b) {
         return a / b;
     }
 
