be33dd1830e01aca32aa913c35dbc3b979bc28a0
Sandeep Rajput <github@rajputsandeep.com>
2018-06-11 18:19:09 +0530

    Corrected typo in testingClass.java file at src/test/java

--- src/test/java/testingClass.java
+++ src/test/java/testingClass.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class testingClass {
 
     @Test
-    public void duringMathod(){
+    public void duringMethod(){
         Assertions.assertEquals(5, Calculator.addition(2,3));
         Assertions.assertEquals(-1, Calculator.subtraction(2,3));
         Assertions.assertEquals(6, Calculator.multiplication(2,3));
