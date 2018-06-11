3f0b8b15c927da2955429e4657f35251c7c0b989
Sandeep Rajput <github@rajputsandeep.com>
2018-06-11 16:30:26 +0530

    Added whitespace to every '{' in calculator.java file

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -1,18 +1,18 @@
 public class calculator {
 
-    static int addition(int a, int b){
+    static int addition(int a, int b) {
         return a + b;
     }
 
-    static int subtraction(int a, int b){
+    static int subtraction(int a, int b) {
         return a - b;
     }
 
-    static int multiplication(int a, int b){
+    static int multiplication(int a, int b) {
         return a * b;
     }
 
-    static int division(int a, int b){
+    static int division(int a, int b) {
         return a / b;
     }
 
