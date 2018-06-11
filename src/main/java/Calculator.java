67f9554a1f310ca0f2abc362643daec0b4ba225d
Sandeep Rajput <github@rajputsandeep.com>
2018-06-11 17:23:37 +0530

    Added comments in Calculator.java file of src/main/java/

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,21 @@
 class Calculator {
 
+    /* Method for addition of Two Integers*/
     static int addition(int a, int b) {
         return a + b;
     }
 
+    /* Method for subtraction of Two Integers*/
     static int subtraction(int a, int b) {
         return a - b;
     }
 
+    /*Method for Multiplication of Two Integers*/
     static int multiplication(int a, int b) {
         return a * b;
     }
 
+    /*Method for Division of Two Integers*/
     static int division(int a, int b) {
         return a / b;
     }
