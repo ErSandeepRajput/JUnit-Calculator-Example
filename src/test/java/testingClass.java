736467c2ba6104ff107999229780ffdae6e7d93b
Sandeep Rajput <github@rajputsandeep.com>
2018-06-13 16:42:22 +0530

    Updated testingClass.java class file by saperating math operations

--- src/test/java/testingClass.java
+++ src/test/java/testingClass.java
@@ -4,10 +4,23 @@ import org.junit.Test;
 public class testingClass {
 
     @Test
-    public void duringMethod(){
+    public void additionMethodTest(){
         Assertions.assertEquals(5, Calculator.addition(2,3));
+    }
+
+    @Test
+    public void subtractionMethodTest(){
         Assertions.assertEquals(-1, Calculator.subtraction(2,3));
+    }
+
+    @Test
+    public void multiplicationMethodTest(){
         Assertions.assertEquals(6, Calculator.multiplication(2,3));
+    }
+
+    @Test
+    public void divisionMethodTest(){
+        Assertions.assertEquals(0, Calculator.division(2,3));
     }
 
 }
